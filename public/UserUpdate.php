dd67f8a427bcbf0efaaf7b6b5764fb5b4ebd9acb
Brandon Kissam <brandon.kissam@gmail.com>
2018-10-20 20:57:47 -0600

    Added comments to the UserUpdate class.

--- public/UserUpdate.php
+++ public/UserUpdate.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * Handles user information received from the frontend and calls the 
+ * planning center API to update a the user.
+ */
 class UserUpdate {
   private $map = array (
     'attend-weekly' => '196809',
@@ -25,6 +28,10 @@ class UserUpdate {
     )
   );
 
+  /**
+   * Sets the keys, secrets and the user's email/id
+   * @constructor
+   */
   public function __construct($pluginName, $email){
     $options = get_option($pluginName);
     $this->appId = $options['app-id'];
@@ -34,14 +41,33 @@ class UserUpdate {
     $this->getUserId();
   }
 
+  /**
+   * Sets the field definition for the Planning Center API.
+   * @param {String} value the id to set
+   * @returns {void}
+   */
   public function setFieldDef($value) {
     $this->data['data']['relationships']['field_definition']['data']['id'] = $this->map[$value];
   }
 
+  /**
+   * Sets the answer to the question and sets it in the Planning Center API
+   * format.
+   * 
+   * @param {String} value Yes|No
+   * @return {void}
+   */
   public function setValue($value) {
     $this->data['data']['attributes']['value'] = $value;
   }
 
+  /**
+   * Sends the user information to the Planning Center API server
+   * and sets the rest of the household if it was selected.
+   * 
+   * @param {Object} postInfo The POST body from the client form
+   * @return {void}
+   */
   public function updateUser($postInfo) {
     $this->sendUserInfo($this->id, $postInfo);
 
@@ -50,6 +76,14 @@ class UserUpdate {
     }
   }
 
+  /**
+   * Sets the values for the body of the POST request to the Planning Center
+   * API and then sends it.
+   * 
+   * @param {String} id The user ID from Planning Center
+   * @param {Object} postInfo The POST body from the client form
+   * @return {void}
+   */
   private function sendUserInfo($id, $postInfo) {
     foreach($postInfo as $key => $value) {
       if($key == 'email') {
@@ -64,6 +98,12 @@ class UserUpdate {
     }
   }
 
+  /**
+   * Sends the actual POST request to the Planning Center API.
+   * 
+   * @param {String} id the user ID from Planning Center
+   * @return {void}
+   */
   private function postUpdate($id) {
     $url = 'https://api.planningcenteronline.com/people/v2/people/' . $id . '/field_data';
     $result = wp_remote_post($url, array(
@@ -82,6 +122,11 @@ class UserUpdate {
     }
   }
 
+  /**
+   * Gets the user ID from Planning Center.
+   * 
+   * @return {String}
+   */
   private function getUserId() {
     $url = 'https://api.planningcenteronline.com/people/v2/people?where[search_name_or_email]=' . $this->email . '&include=households';
     $result = wp_remote_get($url, array(
@@ -95,6 +140,12 @@ class UserUpdate {
     $this->householdId = $json->included[0]->id;
   }
 
+  /**
+   * Loops through the members in a person's household and sends POST body from
+   * the client form.
+   * 
+   * @param {Object} postInfo The POST body from the client form
+   */
   private function setHouseholdMembers($postInfo) {
     $this->household = array();
 
